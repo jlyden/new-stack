39bf19bbbb846900a62918ef41e6d05b73074b5a
jjl <jlyden1@alumni.nd.edu>
2018-01-06 19:46:14 -0500

    Update gitignore and db.sample

--- nodejs/noteapp/app/routes/index.js
+++ nodejs/noteapp/app/routes/index.js
@@ -3,5 +3,3 @@ const noteRoutes = require('./note_routes');
 module.exports = function(app, db) {
   noteRoutes(app,db);
 };
-
-// sample id - 5a511e0561e28a077e16bffd
---
+++ nodejs/noteapp/config/db.sample.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : 'mongodb://<dbuser>:<password>@ds239387.mlab.com:39387/<dbname>'
+};
