db1f4c184d637838f281dd7be38996ebf81fc2c5
jenny lyden <jennifer.lyden@powerschool.com>
2019-12-17 14:58:30 -0500

    Module 2 of tutorial complete

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Santa' },
+    { id: 12, name: 'Dasher' },
+    { id: 13, name: 'Dancer' },
+    { id: 14, name: 'Prancer' },
+    { id: 15, name: 'Vixen' },
+    { id: 16, name: 'Comet' },
+    { id: 17, name: 'Cupid' },
+    { id: 18, name: 'Donner' },
+    { id: 19, name: 'Blitzen' },
+    { id: 20, name: 'Rudolph' }
+];
